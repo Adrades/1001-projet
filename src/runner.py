8b980ebe1ddda7ebcd036358c7603d652201c94e
Adrades <genadrades@gmail.com>
2023-01-22 22:08:15 +0100

    Use fasta file format

--- src/runner.py
+++ src/runner.py
@@ -1,19 +1,31 @@
-from src.loader import ProtLoader
 from src.edit_distance import nvpd
 from sys import argv
 
+import fastaparser as fp
 import os
 
 # TODO real parser ?
 def prot_parser():
+    # use Fasta Parser
     if len(argv) != 3:
         print("You must launch the program with two file names.")
         print(f"Program launched with arguments: {argv}")
-    p1 = ProtLoader(argv[1])
-    p2 = ProtLoader(argv[2])
-    print(f"Sequence 1: {p1.name}({len(p1.prot)})")
-    print(f"Sequence 2: {p2.name}({len(p2.prot)})")
-    return p1.prot, p2.prot
+
+    with open(argv[1], "r")as file:
+        p1 = fp.Reader(file)
+        for seq in p1:
+            seq1 = seq
+            print(f"Sequence 1: {seq.id}({len(seq.sequence_as_string())})")
+            break
+
+    with open(argv[2], "r")as file:
+        p2 = fp.Reader(file)
+        for seq in p2:
+            seq2 = seq
+            print(f"Sequence 2: {seq.id}({len(seq.sequence_as_string())})")
+            break
+
+    return seq1.sequence_as_string(), seq2.sequence_as_string()
 
 
 def run():
